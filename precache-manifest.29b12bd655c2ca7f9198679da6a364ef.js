52b14154d823420ae81df494c5bbd3668a0c18bb
Danilo Rodrigo Cavalcante Bandeira <danilo.rodrigo@live.com>
2019-07-28 03:17:34 -0300

    Updates

---
+++ precache-manifest.29b12bd655c2ca7f9198679da6a364ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b9abe74b72066cf654725a791efa2c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4755d8e6b042a4ca67ed",
+    "url": "/monsters-rolodex/static/css/main.47f2af45.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4755d8e6b042a4ca67ed",
+    "url": "/monsters-rolodex/static/js/main.68f7aeea.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
